b485eb47e34f4e918a6a7c3cf54ea5f303aca0f9
Pereiro, Delfin <delfin.pereiro@rakuten.com>
2016-06-19 13:45:24 +0200

    Match detail header adjustments

--- marcadores/ViewControllers/MatchDetail/MatchDetailHeaderViewViewController.swift
+++ marcadores/ViewControllers/MatchDetail/MatchDetailHeaderViewViewController.swift
@@ -11,12 +11,17 @@ import GSKStretchyHeaderView
 
 class MatchDetailHeaderViewViewController: GSKStretchyHeaderView {
 
+    private let maxFilterAlpha : CGFloat = 0.8
+    private let maxContainerHeight : CGFloat = 80.0
+    private let minContainerHeight : CGFloat = 50.0
+    
     @IBOutlet weak var homeTeamLogoImageView: UIImageView!
     @IBOutlet weak var visitorTeamLogoImageView: UIImageView!
     @IBOutlet weak var filterView: UIView!    
     @IBOutlet weak var teamsNamesContainerView: UIView!
     @IBOutlet weak var homeTeamNameLabel: UILabel!
     @IBOutlet weak var visitorTeamNameLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
     
     @IBOutlet weak var containerViewConstraintHeight: NSLayoutConstraint!
     
@@ -25,20 +30,21 @@ class MatchDetailHeaderViewViewController: GSKStretchyHeaderView {
         self.maximumContentHeight = 136.0
         self.minimumContentHeight = 64.0
         filterView.backgroundColor = UIColor.marcadoresGreen()
-        filterView.alpha = 0.5
+        filterView.alpha = maxFilterAlpha
+        statusLabel.alpha = 0.6
     }
     
     override func didChangeStretchFactor(stretchFactor: CGFloat) {
-        var alpha = CGFloatInterpolate(stretchFactor, 1.0, 0.8)
-        alpha = max(0.80, min(1, alpha));
+        var alpha = CGFloatInterpolate(stretchFactor, 1.0, maxFilterAlpha)
+        alpha = max(maxFilterAlpha, min(1, alpha));
         filterView.alpha = alpha
         
         var teamsNamesAlpha = CGFloatInterpolate(stretchFactor, 0.0, 1.0)
         teamsNamesAlpha = max(0, min(1, teamsNamesAlpha));
         teamsNamesContainerView.alpha = teamsNamesAlpha
         
-        var containerHeight = CGFloatInterpolate(stretchFactor, 50.0, 80.0)
-        containerHeight = max(50.0, min(80.0, containerHeight))
+        var containerHeight = CGFloatInterpolate(stretchFactor, minContainerHeight, maxContainerHeight)
+        containerHeight = max(minContainerHeight, min(maxContainerHeight, containerHeight))
         containerViewConstraintHeight.constant = containerHeight
         self.layoutIfNeeded()
     }
@@ -49,5 +55,6 @@ class MatchDetailHeaderViewViewController: GSKStretchyHeaderView {
         visitorTeamLogoImageView.sd_setImageWithURL(NSURL(string: match.visitorTeam.logoUrl))
         homeTeamNameLabel.text = match.homeTeam.name
         visitorTeamNameLabel.text = match.visitorTeam.name
+        statusLabel.text = match.statusShortDesc
     }
 }
