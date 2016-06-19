11aa5cfcc178b20f99959919fa3c0ed225b9c941
Pereiro, Delfin <delfin.pereiro@rakuten.com>
2016-06-19 22:43:47 +0200

    Adds Team class tests

---
+++ Pods/Target Support Files/Pods-marcadoresTests/Pods-marcadoresTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_marcadoresTests : NSObject
+@end
+@implementation PodsDummy_Pods_marcadoresTests
+@end
---
+++ Pods/Target Support Files/Pods-marcadoresTests/Pods-marcadoresTests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_marcadoresTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_marcadoresTestsVersionString[];
+
